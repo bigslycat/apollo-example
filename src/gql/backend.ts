41f5b0418e1646a89f4310d22614dc88c3e63b02
Pavel Tereschenko <me@stupidcat.wtf>
2023-03-13 01:01:11 +0000

    chore: cleanup

--- src/gql/backend.ts
+++
@@ -1,8 +0,0 @@
-const DB_VERSION = 1
-
-const getDB = (name: string, version: number) =>
-  new Promise<IDBDatabase>((resolve, reject) => {
-    const dbRequest = indexedDB.open(name, version)
-    dbRequest.addEventListener('success', () => resolve(dbRequest.result))
-    dbRequest.addEventListener('error', () => reject(dbRequest.error))
-  })
